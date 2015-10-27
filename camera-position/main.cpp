2c3150254e75b8cec2a7bbf51359bcbefb20d62a
yksz <user4git@gmail.com>
2015-10-27 18:23:15 +0900

    Update the camera-position project

--- camera-position/main.cpp
+++ camera-position/main.cpp
@@ -1,50 +1,97 @@
+#include <cassert>
+#include <cstdio>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <getopt.h>
 
 namespace {
 
 static const int kChessPatternRows = 7;
 static const int kChessPatternColumns = 10;
-static const float kChessGridSize = 24.0; // [mm]
+static const std::string kClickWindowName = "Click image points";
+
+enum EstimationMode {
+    kDefault,
+    kChessPattern,
+};
+
+cv::Mat* clickImage = NULL;
+std::size_t maxClickedCount = 0;
+std::vector<cv::Point2f>* clickedPoints = NULL;
 
 /**
- * ファイルからカメラの内部パラメータを読み込みます。
+ * ファイルから物体座標空間における物体上の点座標を読み込みます。
  *
  * @param[in] filename ファイル名
- * @param[out] intrinsic カメラの内部パラメータ行列
- * @param[out] distortion 歪み係数ベクトル
+ * @param[out] objectPoints 物体上の点
  * @return 読み込めた場合はtrue、そうでなければfalse
  */
-bool readCameraParameters(const std::string& filename,
-        cv::Mat& intrinsic, cv::Mat& distortion) {
-    cv::FileStorage fs(filename, cv::FileStorage::READ);
-    if (!fs.isOpened()) {
-        std::cerr << "ERROR: Failed to open the file: " << filename << std::endl;
+bool readObjectPoints(const std::string& filename,
+        std::vector<cv::Point3f>& objectPoints) {
+    std::ifstream ifs(filename.c_str());
+    if (!ifs.is_open()) {
+        std::cerr << "ERROR: Failed to open file: " << filename << std::endl;
         return false;
     }
-    fs["intrinsic"] >> intrinsic;
-    fs["distortion"] >> distortion;
-    fs.release();
-    return intrinsic.total() != 0 && distortion.total() != 0;
+    std::string line;
+    while (std::getline(ifs, line)) {
+        int x, y, z;
+        sscanf(line.c_str(), "%d,%d,%d", &x, &y, &z);
+        objectPoints.push_back(cv::Point3f(x, y, z));
+    }
+    return true;
+}
+
+void onMouse(int event, int x, int y, int flags, void* params) {
+    switch (event) {
+        case cv::EVENT_LBUTTONDOWN:
+            assert(clickImage != NULL);
+            assert(clickedPoints != NULL);
+
+            if (clickedPoints->size() >= maxClickedCount) {
+                return;
+            }
+            cv::Point2f point(x, y);
+            clickedPoints->push_back(point);
+            std::cout << "count=" << clickedPoints->size() << ", clicked=" << point << std::endl;
+            cv::circle(*clickImage, point, 5, CV_RGB(255, 0, 0), 3);
+            cv::imshow(kClickWindowName, *clickImage);
+            break;
+    }
 }
 
 /**
- * 物体座標空間における物体上の点座標を読み込みます。
+ * 画像から画像上の対応点を読み込みます。
  *
- * @param[out] objectPoints 物体上の点
+ * @param[in] filename 画像ファイル名
+ * @param[in] numPoints 対応点の数
+ * @param[out] imagePoints 画像上の対応点
+ * @return 読み込めた場合はtrue、そうでなければfalse
  */
-void readObjectPoints(std::vector<cv::Point3f>& objectPoints) {
-    for (int i = 0; i < kChessPatternRows; i++) {
-        for (int j = 0; j < kChessPatternColumns; j++) {
-            cv::Point3f point;
-            point.x = j * kChessGridSize;
-            point.y = i * kChessGridSize;
-            point.z = 0.0;
-            objectPoints.push_back(point);
-        }
+bool readImagePoints(const std::string& filename,
+        int numPoints,
+        std::vector<cv::Point2f>& imagePoints) {
+    cv::Mat image = cv::imread(filename);
+    if (image.data == NULL) {
+        std::cerr << "ERROR: Failed to read image" << filename << std::endl;
+        return false;
     }
+    clickImage = &image;
+    maxClickedCount = numPoints;
+    clickedPoints = &imagePoints;
+
+    cv::namedWindow(kClickWindowName, cv::WINDOW_AUTOSIZE);
+    cv::imshow(kClickWindowName, *clickImage);
+    cv::setMouseCallback(kClickWindowName, onMouse);
+    cv::waitKey(0);
+
+    clickImage = NULL;
+    maxClickedCount = 0;
+    clickedPoints = NULL;
+    return true;
 }
 
 /**
@@ -72,16 +119,20 @@ bool findChessboardCorners(cv::Mat& image, cv::Size& patternSize,
 }
 
 /**
- * 画像から画像上の対応点を読み込みます。
+ * 画像からチェスボード上の対応点を読み込みます。
  *
- * @param[in] filename ファイル名
+ * @param[in] filename 画像ファイル名
  * @param[out] imagePoints 画像上の対応点
  * @return 読み込めた場合はtrue、そうでなければfalse
  */
-bool readImagePoints(const std::string& filename,
+bool readImagePointsOnChessboard(const std::string& filename,
         std::vector<cv::Point2f>& imagePoints) {
     // チェスボードの交点を検出する
     cv::Mat image = cv::imread(filename);
+    if (image.data == NULL) {
+        std::cerr << "ERROR: Failed to read image" << filename << std::endl;
+        return false;
+    }
     cv::Size patternSize(kChessPatternColumns, kChessPatternRows);
     bool found = findChessboardCorners(image, patternSize, imagePoints);
     if (!found) {
@@ -90,45 +141,120 @@ bool readImagePoints(const std::string& filename,
     }
 
     // 検出した交点を表示する
-    std::string windowName = "ChessboardCorners";
-    cv::namedWindow(windowName, CV_WINDOW_AUTOSIZE);
+    std::string windowName = "Chessboard Corners";
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
     cv::drawChessboardCorners(image, patternSize, imagePoints, found);
     cv::imshow(windowName, image);
     cv::waitKey(0);
     return true;
 }
 
-} // unnamed namespace
-
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "usage: "
-                << argv[0]
-                << " <camera parameters file> <image file>\n";
-        return 1;
+/**
+ * ファイルからカメラの内部パラメータを読み込みます。
+ *
+ * @param[in] filename ファイル名
+ * @param[out] intrinsic カメラの内部パラメータ行列
+ * @param[out] distortion 歪み係数ベクトル
+ * @return 読み込めた場合はtrue、そうでなければfalse
+ */
+bool readCameraParameters(const std::string& filename,
+        cv::Mat& intrinsic, cv::Mat& distortion) {
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
+    if (!fs.isOpened()) {
+        std::cerr << "ERROR: Failed to open the file: " << filename << std::endl;
+        return false;
     }
-    std::string cameraParamsFileName(argv[1]);
-    std::string imageFileName(argv[2]);
+    fs["intrinsic"] >> intrinsic;
+    fs["distortion"] >> distortion;
+    fs.release();
+    return intrinsic.total() != 0 && distortion.total() != 0;
+}
+
+/**
+ * 物体座標空間におけるカメラ位置を推定します。
+ *
+ * @param[in] estimationMode 推定処理のモード
+ * @param[in] objectPointsFileName 物体上の点座標が書かれたファイル名
+ * @param[in] imageFileName 対応する物体が写っている画像ファイル名
+ * @param[in] cameraParamsFileName カメラの内部パラメータが書かれたファイル名
+ * @param[out] rvec カメラの回転ベクトル
+ * @param[out] tvec カメラの並進ベクトル
+ * @return 推定できた場合はtrue、そうでなければfalse
+ */
+bool estimateCameraPosition(EstimationMode estimationMode,
+        const std::string& objectPointsFileName,
+        const std::string& imageFileName,
+        const std::string& cameraParamsFileName,
+        cv::Mat& rvec,
+        cv::Mat& tvec) {
+    std::vector<cv::Point3f> objectPoints;
+    if (!readObjectPoints(objectPointsFileName, objectPoints)) {
+        std::cerr << "ERROR: Failed to read object points\n";
+        return false;
+    }
+
+    std::vector<cv::Point2f> imagePoints;
+    bool result = false;
+    switch (estimationMode) {
+        case kChessPattern:
+            result = readImagePointsOnChessboard(imageFileName, imagePoints);
+            break;
+
+        case kDefault:
+            result = readImagePoints(imageFileName, objectPoints.size(), imagePoints);
+            break;
+
+        default:
+            assert(false && "Unknown mode");
+    }
+    if (!result) {
+        std::cerr << "ERROR: Failed to read image points\n";
+        return false;
+    }
 
     cv::Mat intrinsic, distortion;
     if (!readCameraParameters(cameraParamsFileName, intrinsic, distortion)) {
         std::cerr << "ERROR: Failed to read camera parameters\n";
-        return 1;
+        return false;
     }
 
-    std::vector<cv::Point3f> objectPoints;
-    readObjectPoints(objectPoints);
+    cv::solvePnP(objectPoints, imagePoints, intrinsic, distortion, rvec, tvec);
+    return true;
+}
 
-    std::vector<cv::Point2f> imagePoints;
-    if (!readImagePoints(imageFileName, imagePoints)) {
-        std::cerr << "ERROR: Failed to read image points\n";
+} // unnamed namespace
+
+int main(int argc, char** argv) {
+    std::string mode;
+    int opt;
+    while ((opt = getopt(argc, argv, "m:")) != -1) {
+        switch (opt) {
+            case 'm':
+                mode = std::string(optarg);
+                break;
+        }
+    }
+    if (argc - optind < 3) {
+        std::cerr << "usage: "
+                << argv[0]
+                << " [options]"
+                " <object points file> <image file> <camera parameters file>\n"
+                " -m\tmode ('chess' or default)"
+                << std::endl;
         return 1;
     }
+    EstimationMode estimationMode = mode == "chess" ? kChessPattern : kDefault;
+    std::string objectPointsFileName(argv[optind + 0]);
+    std::string imageFileName(argv[optind + 1]);
+    std::string cameraParamsFileName(argv[optind + 2]);
 
     cv::Mat rvec, tvec;
-    cv::solvePnP(objectPoints, imagePoints, intrinsic, distortion, rvec, tvec);
-
-    std::cout << "rvec: \n" << rvec << std::endl;
-    std::cout << "tvec: \n" << tvec << std::endl;
-    return 0;
+    bool result = estimateCameraPosition(estimationMode,
+            objectPointsFileName,
+            imageFileName,
+            cameraParamsFileName,
+            rvec, tvec);
+    std::cout << "rvec:\n" << rvec << std::endl;
+    std::cout << "tvec:\n" << tvec << std::endl;
+    return result;
 }
