ec7b1fdf1b7001c2a885716328bceb3c8b7f4251
yksz <user4git@gmail.com>
2015-10-30 15:02:55 +0900

    Refactor 3D-view/main.c

--- 3D-view/main.c
+++ 3D-view/main.c
@@ -4,11 +4,11 @@
 #include <GL/glut.h>
 #include "logger.h"
 
+#define PERSPECTIVE_ENABLED
+
 #define M_PI 3.14159265358979323846
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
-
-#define PERSPECTIVE_ENABLED
 
 typedef struct
 {
